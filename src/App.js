0ee513d04714bd5fd17f8d004331f6dc44fbaa77
Guaguagugu <batato4ni@gmail.com>
2023-03-16 00:52:00 +0800

    create sth 0315

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+// import Nav from "./components/Nav";
+// import Footer from "./components/Footer";
+import Homepage from "./pages/Homepage";
+import "./styles/all.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* <Nav /> */}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+      {/* <Footer /> */}
+    </div>
+  );
 }
 
 export default App;
