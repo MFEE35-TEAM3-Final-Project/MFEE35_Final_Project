7bad43b5d6b4746793a8fe4d02d16390823a72b9
Guaguagugu <batato4ni@gmail.com>
2023-03-17 01:38:39 +0800

    fix img src

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,11 +8,43 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 function HomepageNav() {
   return (
     <div id="header">
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="/">
-            <img src={"/img/Kessoku_Band_Logo.svg"} alt="" />
-          </Navbar.Brand>
+      <Nav className="navbar navbar-expand-lg bg-body-tertiary p-0">
+        <div className="w-100">
+          <div className="nav_head">
+            <a
+              className="navbar-brand me-auto me-lg-0 col-lg-1 ps-lg-4"
+              href="/"
+            >
+              <img src={"/img/Kessoku_Band_Logo.svg"} alt="" />
+            </a>
+            <div className="d-none d-lg-flex justify-content-lg-around col-lg-11">
+              <div className="nav-item">
+                <a href="#intro_selection" className="nav-link">
+                  INTRODUCTION
+                </a>
+              </div>
+              <div className="nav-item">
+                <a href="#live_section" className="nav-link">
+                  LIVE
+                </a>
+              </div>
+              <div className="nav-item">
+                <a href="#picture_section" className="nav-link">
+                  PICTURE
+                </a>
+              </div>
+              <div className="nav-item">
+                <a href="#sns_section" className="nav-link">
+                  SNS~
+                </a>
+              </div>
+              <div className="nav-item">
+                <a href="https://bocchi.rocks/" className="nav-link">
+                  OFFICIAL
+                </a>
+              </div>
+            </div>
+          </div>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
@@ -33,8 +65,8 @@ function HomepageNav() {
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
-        </Container>
-      </Navbar>
+        </div>
+      </Nav>
     </div>
   );
 }
