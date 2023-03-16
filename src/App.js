35750572f13f08209db4de494958f985b52ca64b
AAAwawa <batato4ni@gmail.com>
2023-03-16 16:34:58 +0800

    03016

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-// import Nav from "./components/Nav";
+import Nav from "./components/Nav";
 // import Footer from "./components/Footer";
 import Homepage from "./pages/Homepage";
 import "./styles/all.css";
@@ -8,7 +8,7 @@ import "./styles/all.css";
 function App() {
   return (
     <div className="App">
-      {/* <Nav /> */}
+      <Nav />
       <Routes>
         <Route path="/" element={<Homepage />} />
       </Routes>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <div className="fs-1 text-white  ">Homepage</div>
+   
+    </div>
+  );
 };
 
 export default Homepage;
