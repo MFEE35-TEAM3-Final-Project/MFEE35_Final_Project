f07f43a15933b3284f8df93c71b00f684a1ba964
tingjiangye <tingjiangye@gmail.com>
2023-05-12 16:41:06 +0800

    fix merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import Nav from "./components/Nav";
-import "./styles/Nav.css";
-// import Footer from "./components/Footer";
-// import HomePage from "./pages/HomePage";
 
+// Component
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+// Pages
+import HomePage from "./pages/HomePage";
+import Blog from "./pages/blog";
+import Article from "./pages/Article";
+import Calculator from "./pages/Calculator";
 import StorePage from "./pages/StorePage";
 import GoodsPage from "./pages/GoodsPage";
 import ShoppingcartPage from "./pages/ShoppingcartPage";
 
 import "./styles/all.css";
-import Footer from "./components/Footer";
-import HomePage from "./pages/HomePage";
+
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import UserProfiles from "./pages/user/UserProfile";
@@ -19,9 +22,7 @@ import UserSelfies from "./pages/user/UserSelfies";
 import UserDashboard from "./pages/user/UserDashboard";
 // import Articles from "./pages/admin/Article";
 import Food from "./pages/user/Food";
-import Blog from "./pages/blog";
-import Calculator from "./pages/Calculator";
-import Article from "./pages/Article";
+
 // style
 import "./styles/member/main.css";
 import "./styles/member/MemberHeader.css";
@@ -30,7 +31,6 @@ import "./styles/member/user_foodRecord.css";
 import "./styles/member/memberHome.css";
 import "./styles/member/userinfo.css";
 import "./styles/member/chartlist.css";
-import "bootstrap/dist/css/bootstrap.css";
 
 // Route
 import MemberLoginPage from "./pages/MemberLoginPage";
@@ -57,29 +57,20 @@ function App() {
           <Route path="/calculator" element={<Calculator />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/article/:id" element={<Article />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          {/* user page */}
-          <Route path="/articles" element={<Articles />} />
-          <Route path="/food" element={<Food />} />
-          <Route element={<UserDashboard />}>
-            <Route path="/user/profile" element={<UserProfiles />} />
-            <Route path="/user/selfies" element={<UserSelfies />} />
-          </Route>
-          <Route path="/admin/login" element={<AdminLogin />} />
-          <Route element={<AdminBoard />}>
-            <Route path="/admin/backstage" element={<BackStage />} />
-          </Route>
-          <Route path="/articles" element={<Articles />} />
-          <Route path="/food" element={<Food />} />
-          <Route path="/foodRecord" element={<FoodRecordNumber />} />{" "}
-          <Route path="/login" element={<LoginPage />} />
           <Route path="/store" element={<StorePage />} />
           <Route
             path="/goods/:productId/:activityid/:foodId"
             element={<GoodsPage />}
           />
-          <Route path="/cart" element={<ShoppingcartPage />} />{" "}
+          <Route path="/cart" element={<ShoppingcartPage />} />
+          <Route path="/foodRecord" element={<FoodRecordNumber />} />
+
+          <Route element={<AdminBoard />}>
+            <Route path="/admin/backstage" element={<BackStage />} />
+          </Route>
+
+          {/* user page */}
+
           <Route path="/LoginPage" element={<MemberLoginPage />} />
           <Route path="/MemberHomePage" element={<MemberHomePage />} />
           <Route path="/MemberRegister1" element={<MemberRegister1 />} />
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap/dist/js/bootstrap.min.js'
+import "bootstrap/dist/js/bootstrap.min.js";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  // <BrowserRouter>
+  //   <App />
+  // </BrowserRouter>
 
-  // <React.StrictMode>
-  //   <BrowserRouter>
-  //     <App />
-  //   </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
