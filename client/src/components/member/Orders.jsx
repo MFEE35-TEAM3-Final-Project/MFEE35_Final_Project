11cedd207610d859d0d4f3981246dc438392ffcb
fz-yang <peter33989@gmail.com>
2023-05-16 14:59:17 +0800

    Merge branch 'fzyang' of https://github.com/MFEE35-TEAM3-Final-Project/MFEE35_Final_Project into fzyang

--- client/src/components/member/Orders.jsx
+++ client/src/components/member/Orders.jsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import "../../styles/member/favoritelist.css";
-import MemberHeader from './MemberHeader';
+import '../../styles/member/orders.css';
+// import MemberHeader from './MemberHeader';
 
 function Orders() {
   const [data, setData] = useState([]);
+  const [expandedOrderId, setExpandedOrderId] = useState(null); // 追踪展開的訂單ID
   const [error, setError] = useState(null);
   const [hasFetched, setHasFetched] = useState(false);
 
@@ -19,24 +20,53 @@ function Orders() {
   const fetchOrderlist = async () => {
     try {
       var jwtToken = document.cookie.split('=')[1].trim();
-      const data = await axios.get(`${process.env.REACT_APP_API_URL}/api/user/orders`, {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/user/orders`, {
         headers: {
-          Authorization: jwtToken
-        }
+          Authorization: jwtToken,
+        },
       });
-      console.log(data)
-      setData(data.data.message);
+      console.log(response);
+      setData(response.data.data || []);
       setHasFetched(true);
     } catch (error) {
       toast.error(error.response.data.message);
     }
   };
 
+  const toggleOrderDetails = (orderId) => {
+    setExpandedOrderId((prevOrderId) => (prevOrderId === orderId ? null : orderId));
+  };
+
   return (
     <div>
       {/* <MemberHeader/> */}
       <ToastContainer />
-      
+      {Array.isArray(data) && data.length > 0 ? (
+        <div className="order-container">
+          {data.map((order, index) => (
+            <div key={index} className={`order-item ${expandedOrderId === order.order_id ? 'expanded' : ''}`}>
+              <div className="order-header" onClick={() => toggleOrderDetails(order.order_id)}>
+                <p>訂單編號：{order.order_id}</p>
+                <p>金額：{order.total_price}</p>
+                {/* 顯示其他訂單相關資訊 */}
+              </div>
+              {expandedOrderId === order.order_id && (
+                <div className="order-details">
+                  {order.order_details.map((detail, detailIndex) => (
+                    <div key={detailIndex} className="order-detail-item">
+                      <p>產品名稱：{detail.name}</p>
+                      <p>數量：{detail.quantity}</p>
+                      {/* 顯示其他訂單詳細資訊 */}
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>沒有可用的訂單資料。</p>
+      )}
     </div>
   );
 }
