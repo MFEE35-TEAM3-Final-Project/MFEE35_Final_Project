6e7c8a9270dbc2a85733d4482c362f1518d6b6a3
Guaguagugu <batato4ni@gmail.com>
2023-05-21 23:47:40 +0800

    i don't know

--- client/src/service/UploadAdapterPlugin.js
+++ client/src/service/UploadAdapterPlugin.js
@@ -1,36 +1,48 @@
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, message, Upload } from "antd";
 import axios from "axios";
-import { v2 as cloudinary } from "cloudinary";
 
-const UploadAdapterPlugin = (editor) => {
-  editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
-    return {
-      upload: () => {
-        return new Promise((resolve, reject) => {
-          loader.file.then((file) => {
-            cloudinary.v2.uploader.upload(
-              file,
-              {
-                upload_preset: "joold_test" // 你的 Cloudinary 上傳預設值
-              },
-              (error, result) => {
-                if (error) {
-                  reject(error);
-                } else {
-                  const imageUrl = result.secure_url;
-                  resolve({
-                    default: imageUrl
-                  });
-                }
-              }
-            );
-          });
-        });
-      },
-      abort: () => {
-        // Implement aborting the upload if needed
+const CloudinaryUpload = () => {
+  const handleUpload = async (options) => {
+    const { onSuccess, onError, file } = options;
+    console.log("fil", file);
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", "joold_test"); // 將 "your_cloudinary_preset" 替換為你的Cloudinary上傳預設值
+
+    try {
+      const response = await axios.post(
+        "https://api.cloudinary.com/v1_1/ddh6e9dad/image/upload",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data"
+          }
+        }
+      );
+      console.log("re", response);
+      if (response.data && response.data.secure_url) {
+        onSuccess("success");
+        message.success(`${file.name} uploaded successfully`);
+      } else {
+        onError("error");
+        message.error(`${file.name} upload failed.`);
       }
-    };
+    } catch (error) {
+      onError("error");
+      message.error(`${file.name} upload failed.`);
+    }
   };
+
+  return (
+    <Upload
+      customRequest={handleUpload}
+      accept="image/*"
+      showUploadList={false}
+    >
+      <Button icon={<UploadOutlined />}>Click to Upload</Button>
+    </Upload>
+  );
 };
 
-export default UploadAdapterPlugin;
+export default CloudinaryUpload;
