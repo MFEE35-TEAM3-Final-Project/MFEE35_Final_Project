ca70ccb080d39150a5224d224d4bad39eb26a068
fz-yang <peter33989@gmail.com>
2023-05-23 15:58:45 +0800

    0523 orderupdate

--- client/src/components/member/Orders.jsx
+++ client/src/components/member/Orders.jsx
@@ -50,62 +50,78 @@ function Orders() {
   return (
     <div>
       <MemberHeader />
-      {/* <MemberHeader/> */}
       <ToastContainer />
       {Array.isArray(data) && data.length > 0 ? (
-        <div className="order-container row">
+        <div className="order-container ">
           {data.map((order, index) => (
             <div
               key={index}
-              className={`order-item ${expandedOrderId === order.order_id ? "expanded" : ""
+              className={`order-item ${expandedOrderId === order.order_id ? "expanded " : ""
                 }`}
             >
-              <div className="order-header">
-                {order.order_details.length > 0 && (
-                  <div className="order-detail-item">
+
+              {order.order_details.length > 0 && (
+
+                <div className="order-detail-item row">
+                  <div className="col-12 order-detail">
+                    <h1 className="status">訂單狀態：{order.status}</h1>
+                    <p>商品數量：{order.total_quantity}</p>
+                    <p>訂單金額：{order.total_price}</p>
+                    {expandedOrderId === order.order_id && (
+                      <div className="order-item expanded">
+                        <p>寄送資訊</p>
+                      </div>
+                    )}
+                  </div>
+                  <a className=" order_list row" href={`/goods/${order.order_details[0].productid}/${order.order_details[0].activityId}/${order.order_details[0].food_id}`}>
                     <img
+                      className="col-4"
                       src={order.order_details[0].image[0]}
                       alt={order.order_details[0].name}
-                      className="col-3"
                     />
-                    <div className="col-6">
-                      <p>產品名稱：{order.order_details[0].name}</p>
-                      <p>數量：{order.order_details[0].quantity}</p>
+
+                    <div className="col-8">
+                      <h2>{order.order_details[0].name}</h2>
+                      <p>x{order.order_details[0].quantity}</p>
+                      <p>${order.order_details[0].price}</p>
+                      <button>再買一次</button>
                     </div>
-
-                  </div>
-                )}
-                <div
-                  className={`order-details ${expandedOrderId === order.order_id ? "expanded row" : "row"
-                    }`}
-                >
-                  {expandedOrderId === order.order_id &&
-                    order.order_details.map((detail, detailIndex) => (
-                      <div key={detailIndex} className="order-detail-item">
-
-                        <img className="col-3" src={detail.image[0]} alt={detail.name} />
-                        <div className="col-6">
-                          <p>產品名稱：{detail.name}</p>
-                          <p>數量：{detail.quantity}</p>
+                  </a>
+                  <div
+                    className={` ${expandedOrderId === order.order_id ? "expanded order-detail-item-product" : ""
+                      }`}
+                  >
+                    {expandedOrderId === order.order_id &&
+                      order.order_details.slice(1).map((detail, detailIndex) => (
+                        <div key={detailIndex} className="row order-detail-item-product">
+                          <a className=" order_list row" href={`/goods/${detail.productid}/${detail.activityId}/${detail.food_id}`}>
+                            <img className="col-4" src={detail.image[0]} alt={detail.name} />
+                            <div className="col-8">
+                              <h2>{detail.name}</h2>
+                              <p>x{detail.quantity}</p>
+                              <p>${detail.price}</p>
+                              <button>再買一次</button>
+                            </div>
+                          </a>
                         </div>
-                        {/* 顯示其他訂單詳細資訊 */}
-                      </div>
-                    ))}
+                      ))}
+                  </div>
+                  {order.order_details.length > 1 ? <button className="order-detail-item expand-button" onClick={() => toggleOrderDetails(order.order_id)} > 展開
+                  </button> : <span></span>}
                 </div>
-                <div
-                  className="expand-button"
-                  onClick={() => toggleOrderDetails(order.order_id)}
-                >
-                  展開
-                </div>
-              </div>
+
+
+              )}
             </div>
+
           ))}
         </div>
       ) : (
         <p>沒有可用的訂單資料。</p>
-      )}
-    </div>
+      )
+      }
+    </div >
+
   );
 }
 
